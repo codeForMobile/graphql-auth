379c6b2dc6753fe453ea5d81b879223b56f0e2c1
codeForMobile <code.mobile@protonmail.com>
2023-11-13 19:48:22 -0600

    implement private route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { typeDefs, resolvers } from './graphql';
 import { GqlContext } from './graphql/types';
+import Auth from './modules/auth';
+import { GraphQLError } from 'graphql';
 
 const server = new ApolloServer<GqlContext>({ typeDefs, resolvers });
 
@@ -9,8 +11,19 @@ const port = Number(process.env.PORT ?? 8080);
 const startServer = async () => {
   const { url } = await startStandaloneServer(server, {
     listen: { host: '0.0.0.0', port },
-    context: async () => {
-      return {};
+    context: async ({ req }) => {
+      console.log(req.headers)
+      const authToken = req.headers.authorization?.split(' ')[1]
+      if (!authToken) return { currentUser: null };
+      try {
+        const user = await Auth.verifyAuthToken(authToken)
+        console.log('currentUser', user)
+        return { currentUser: user}
+      } catch (_) {
+        throw new GraphQLError('Invalid auth token', {
+          extensions: {code: 'Not_Authorized'}
+        })
+      }
     },
   });
   console.log(`Server running at ${url}`);
